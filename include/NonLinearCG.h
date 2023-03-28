d77b58a2201ced0eb3ec9d864cfab011ca2f6980
Wakim B. Saba <54562008+wakimbsaba@users.noreply.github.com>
2023-03-28 14:56:26 -0300

    Update NonLinearCG.h

--- include/NonLinearCG.h
+++ include/NonLinearCG.h
@@ -7,6 +7,8 @@
 ///  \author    Wakim B. Saba
 [[ http://lattes.cnpq.br/6231635928638396 | Lattes Page ]]
 <a href="https://br.linkedin.com/in/wakim-saba-9940445">(linkedin)</a>
+[Conjugate gradient method](<https://en.wikipedia.org/wiki/Conjugate_gradient_method> "Wikipedia:Conjugate gradient method")
+
 ///
 ///  \version   1.0.0.0
 ///
