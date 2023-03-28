c71d08647791907ca83ba42b862d0a1f1fef1213
Wakim B. Saba <54562008+wakimbsaba@users.noreply.github.com>
2023-03-28 14:55:31 -0300

    Update NonLinearCG.h

--- include/NonLinearCG.h
+++ include/NonLinearCG.h
@@ -6,7 +6,7 @@
 ///
 ///  \author    Wakim B. Saba
 [[ http://lattes.cnpq.br/6231635928638396 | Lattes Page ]]
-///  <a href="https://br.linkedin.com/in/wakim-saba-9940445">(linkedin)</a>
+<a href="https://br.linkedin.com/in/wakim-saba-9940445">(linkedin)</a>
 ///
 ///  \version   1.0.0.0
 ///
